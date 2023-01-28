a6a30f696c9cdc839f02d761de1c7e8f872f3fba
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-01-28 23:27:27 +0530

    No changes

---
+++ beautyhub/components/blogsArray.ts
@@ -0,0 +1,135 @@
+export interface Blogs {
+    href:String,
+    desc:String,
+    heading:String
+}
+
+const blogs: Blogs[] = [
+    {
+      "href": "https://www.futurederm.com/",
+      "desc": "A go-to resource for in-depth, well-researched information about beauty, fashion, nutrition, and modern lifestyle from award-winning blogger Nicki Zevola. They explain the science behind beauty products and ingredients.",
+      "heading": "FutureDerm"
+    },
+    {
+      "href": "http://www.pammyblogsbeauty.com/",
+      "desc": "Here you will find detailed and upbeat reviews of beauty products and fashion trends. Author Pammy believes that beauty and fashion are fun and should not be taken too seriously.",
+      "heading": "Pammy Blogs Beauty"
+    },
+    {
+      "href": "http://lipsticklifestyle.com/",
+      "desc": "Canada's up-to-the-minute online authority for travel, spas, beauty, and health.",
+      "heading": "Lipstick Lifestyle"
+    },
+    {
+      "href": "http://www.totalbeauty.com/",
+      "desc": "Brings you expert advice, how-to tips and tricks, beauty samples and deals, and thousands of unbiased beauty product reviews. They are committed to bringing you unbiased beauty advice and always strive to expand their knowledge and experience in order to be the smartest, most comprehensive, and useful resource for all things beauty.",
+      "heading": "Total Beauty"
+    },
+    {
+      "href": "http://www.beautyjunkiesunite.com/WP/",
+      "desc": "Beauty community with giveaways, product reviews, makeup swatches, the latest product release info, blog posts, and more.",
+      "heading": "Beauty Junkies Unite"
+    },
+    {
+      "href": "http://www.thebeautyoflifeblog.com/",
+      "desc": "One of the fore-running beauty blogs in the industry. Run by Editor-in-Chief, Jamie Allison Sanders, The Beauty of Life includes product reviews, interviews with heavy hitters in the beauty industry, beauty news and more.",
+      "heading": "The Beauty of Life"
+    },
+    {
+      "href": "http://www.15minutebeauty.com/",
+      "desc": "A beauty blog about quick  tips, product reviews and getting ready in a flash.",
+      "heading": "15 Minute Beauty Fanatic"
+    },
+    {
+      "href": "http://www.atouchofblusher.com/",
+      "desc": "Provides latest news and reviews of makeup, skincare, and fragrance products. Focuses on Japanese cosmetics as well as western beauty products.",
+      "heading": "A Touch of Blusher"
+    },
+    {
+      "href": "http://www.musingsofamuse.com/",
+      "desc": "Dedicated to beauty junkies that features reviews of beauty products from both the US and Asia.",
+      "heading": "Musings of a Muse"
+    },
+    {
+      "href": "http://www.alllacqueredup.com/",
+      "desc": "A nail polish and beauty blog featuring swatches, nail art, tutorials, reviews and trends plus manicure and pedicure tips and tricks.",
+      "heading": "All Lacquered Up"
+    },
+    {
+      "href": "http://themakeupblogger.com/",
+      "desc": "A humorous and honest take on beauty products, lifestyle and confidence tips as well as makeup tricks from professional contributors that help you learn how to make makeup application easy and fun. Main goal is to convey that beauty is something that comes from within. Your smile, the sparkle in your eyes, and the joy in your heart is what will make you gorgeous. Find the feature that you love about yourself and enhance that! Beauty is different for everyone, and being unique is what makes us special.",
+      "heading": "The Makeup Blogger"
+    },
+    {
+      "href": "http://www.norahlovesmakeup.com/",
+      "desc": "Here you will find tips on makeup, skincare, hair, and lifestyle.",
+      "heading": "Norah Loves Makeup"
+    },
+    {
+      "href": "http://simplysona.com/",
+      "desc": "Learn the latest trends in Beauty from a professional MUA turned Beauty and Fashion Blogger on how to bring out the best version of you. Sona is a professional makeup artist who started creating YouTube videos to share her passion of makeup and beauty skills with others.",
+      "heading": "Simply Sona"
+    },
+    {
+      "href": "http://michellephan.com/category/beauty/",
+      "desc": "Michelle a popular makeup artist who is passionate about teaching others how to look and feel fabulous in their own skin.",
+      "heading": "Michelle Phan"
+    },
+    {
+      "href": "https://www.zoella.co.uk/category/beauty/make-up",
+      "desc": "Author Zoe Sugg is enthusiastic about all things beauty, fashion and life. Covers beauty, fashion, life stories and some other controversial & health subjects thrown in for good measure.",
+      "heading": "Zoella"
+    },
+    {
+      "href": "http://www.thebeautybybel.com/search/label/makeup",
+      "desc": "Mission is to help every girl feel beautiful from the inside out.",
+      "heading": "The Beauty Bybel"
+    },
+    {
+      "href": "http://dulcecandy.com/",
+      "desc": "Includes everything from DIY makeup to nail, hair and lifestyle tutorials.",
+      "heading": "Dulce Candy"
+    },
+    {
+      "href": "http://heyfranhey.com/",
+      "desc": "Author Francheska has been described as the DIY, healthy hair products and haircare queen.",
+      "heading": "Hey Fran Hey"
+    },
+    {
+      "href": "http://www.misslizheart.com/search/label/beauty",
+      "desc": "Covers fashion, hair & makeup. Also includes great beauty and DIY tips.",
+      "heading": "Miss Liz Heart"
+    },
+    {
+      "href": "http://grittypretty.com/category/makeup/",
+      "desc": "Gritty Pretty is a website dedicated to beauty. It is for the woman who is confident and assertive in her own style but wants to learn more about beauty trends, techniques, products and innovations.",
+      "heading": "Gritty Pretty"
+    },
+    {
+      "href": "http://www.thesundaygirl.com/",
+      "desc": "UK based beauty website that is updated frequently and offers in-depth, honest but neutral reviews of beauty products of any nature -hair, cosmetics, skin care, nails, etc.",
+      "heading": "The Sunday Girl"
+    },
+    {
+      "href": "http://beautybanter.com/",
+      "desc": "Beauty Banter was launched in July of 2006 as a comprehensive beauty blog covering trends, tips and tricks, insider secrets, and weekly must-haves. Beauty Banter has a reputation of being on the cutting edge of emerging trends and product launches so our readers are always the first to know what’s hot and what’s just not. Their mission is to form a dialogue between reader and expert — to merge runway trends with real life, and to give our readers a VIP peak into the world of beauty through the people who shape that very art form. You’ll always get exclusive content, always interesting, and, hopefully, always inspiring.",
+      "heading": "Beauty Banter"
+    },
+    {
+      "href": "http://politicsofpretty.com/",
+      "desc": "A D.C beauty blog dedicated to all things hair, makeup, skin, and nails.",
+      "heading": "Politics of Pretty"
+    },
+    {
+      "href": "http://www.organicbeautytalk.com/",
+      "desc": "Organic Beauty Talk was created in May 2011 out of a love and passion for knowing what’s in your products, caring about what goes on your skin, and connecting you with brands that have the best organic, natural and eco-friendly choices. Organic Beauty Talk is dedicated to helping you live life more organically and beautifully.",
+      "heading": "Organic Beauty Talk"
+    },
+    {
+      "href": "http://thecleanbeautyblog.tumblr.com/",
+      "desc": "The Clean Beauty Blog is about beauty at its purest. It’s about products without the parabens, without the fillers, and without the synthetic chemicals. It’s about makeup and skincare that is good for you (i.e. works!), good for your health, and good for the environment. It was created to fill a gap in the beauty blogosphere: it fuses the wholesome ethos of wellness blogs and natural & organic beauty sites with the sophistication, style and trend-based focus of conventional beauty websites and magazines. The blog features products ranging from health food shops to luxury cosmetics and skincare, and includes news, reviews, how-tos and trend spotting.",
+      "heading": "The Clean Beauty Blog"
+    }
+];
+
+export default blogs;
