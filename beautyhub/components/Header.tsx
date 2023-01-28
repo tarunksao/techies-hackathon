928f352b0c679f2f7dc0db41334ce548369f7b56
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-01-28 18:29:01 +0530

    Added Navbar with login/regist Drawer and a HomePage(still working on it)

---
+++ beautyhub/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Box, Flex, Heading, Image} from '@chakra-ui/react';
+import CustomImage from './CustomInage';
+import Link from 'next/link';
+import Register from './Register';
+import Login from './Login';
+
+export default function Header() {
+    return (
+        <Flex justifyContent='space-evenly' alignItems='center' py={2} bgColor={'#FF5DB2'} >
+            <Box w='150px' h='75px'>
+                <Image src={'/Images/logo-color(1).png'} alt={'logo'} w='150px' h='75px' />
+            </Box>
+            <Register />
+            <Login />
+        </Flex>
+    )
+};
