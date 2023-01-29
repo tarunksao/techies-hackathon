a17a56c39b7d3042be1aadcc146a24af2c956b65
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-01-29 05:57:09 +0530

    Added Login/Signup Drawers (Styled)

--- beautyhub/components/Header.tsx
+++ beautyhub/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Flex, Heading, Image} from '@chakra-ui/react';
+import {Box, Button, Flex, Heading, Image} from '@chakra-ui/react';
 import CustomImage from './CustomInage';
 import Link from 'next/link';
 import Register from './Register';
@@ -9,12 +9,16 @@ export default function Header() {
     return (
         <>
         <Box  pos={'fixed'} top={0} w='full' >
-            <Flex justifyContent='space-evenly' alignItems='center' py={2} bgColor={'#FF5DB2'} >
-                <Box w='150px' h='75px'>
-                    <Image src={'/Images/logo-color(1).png'} alt={'logo'} w='150px' h='75px' />
+            <Flex justifyContent='space-around' alignItems='center' py={2} bgColor={'#FF5DB2'} >
+                <Box w='180px' h='75px'>
+                    <Image src={'/Images/logo-color(1).png'} alt={'logo'} w='180px' h='75px' />
                 </Box>
-                <Register />
-                <Login />
+                <Button colorScheme={'yellow'}>All Videos</Button>
+                <Button colorScheme={'yellow'}>About Us</Button>
+                <Flex gap={10}>
+                    <Register />
+                    <Login />
+                </Flex>
             </Flex>
         </Box>
         </>
