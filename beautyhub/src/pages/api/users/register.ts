d9b9fbf6edb1daba79d830c62db83342d642a0e5
Tarun Kumar Sao <107460801+tarunksao@users.noreply.github.com>
2023-01-29 19:17:08 +0530

    Update register.ts

--- beautyhub/src/pages/api/users/register.ts
+++ beautyhub/src/pages/api/users/register.ts
@@ -1,9 +1,10 @@
-import connect from "../../../../lib/mongodb"
-import {UserModel} from "../../../../model/Schema"
-connect()
+import connect from "lib/mongodb"
+import {UserModel} from "model/Schema"
+import { NextApiRequest, NextApiResponse } from "next"
 
- const register = async (req,res) => {
+ const register = async (req:NextApiRequest,res:NextApiResponse) => {
     try{
+     await connect()
         const user =  new UserModel(req.body)
         await user.save()
         if(!user){
