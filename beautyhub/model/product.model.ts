4157e3fe9bfaa8244a8b48c42b3a832b9b7ec024
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-01-29 21:24:04 +0530

    Rectified TypeScript part of code & Product page

--- beautyhub/model/product.model.ts
+++ beautyhub/model/product.model.ts
@@ -22,41 +22,5 @@ export interface Product {
     image_src:string;
     imgWidth:number;
     imgHeight:number;
+    _id:string
 }
-
-// [
-//     {
-//         "kind": "youtube#searchResult",
-//         "etag": "KEDbLWpBUoh4gwqA-RJOEtNbAyw",
-//         "id": {
-//             "kind": "youtube#video",
-//             "videoId": "wuXqKL6Fh0M"
-//         },
-//         "snippet": {
-//             "publishedAt": "2023-01-13T17:00:11Z",
-//             "channelId": "UCRXiA3h1no_PFkb1JCP0yMA",
-//             "title": "Julianne Moore’s Guide to Flattering Makeup for Redheads | Beauty Secrets | Vogue",
-//             "description": "Actor Julianne Moore walks us through her everyday skin care routine and reveals the secrets behind her day-to-night makeup ...",
-//             "thumbnails": {
-//                 "default": {
-//                     "url": "https://i.ytimg.com/vi/wuXqKL6Fh0M/default.jpg",
-//                     "width": 120,
-//                     "height": 90
-//                 },
-//                 "medium": {
-//                     "url": "https://i.ytimg.com/vi/wuXqKL6Fh0M/mqdefault.jpg",
-//                     "width": 320,
-//                     "height": 180
-//                 },
-//                 "high": {
-//                     "url": "https://i.ytimg.com/vi/wuXqKL6Fh0M/hqdefault.jpg",
-//                     "width": 480,
-//                     "height": 360
-//                 }
-//             },
-//             "channelTitle": "Vogue",
-//             "liveBroadcastContent": "none",
-//             "publishTime": "2023-01-13T17:00:11Z"
-//         }
-//     }
-// ]
--- beautyhub/src/pages/products/index.tsx
+++ beautyhub/src/pages/products/index.tsx
@@ -1,38 +1,26 @@
-
-
-//https://youtube.googleapis.com/youtube/v3/search?q=beauty&part=snippet&maxResults=20&key=AIzaSyBoSLMZ1EA-pYQ4sQRszn-TZZeAjG0F2Wg
-import { Center } from "@chakra-ui/react"
 import react from "react"
-import { useDispatch, useSelector } from "react-redux"
-import  getProduct  from "../redux-arch/app/action"
-import * as types from "../redux-arch/app/actionType"
-import  getProductapi  from "../redux-arch/app/product.api"
-import  RootState, { useAppDispatch, useAppSelector }  from "../redux-arch/store"
+import getProduct  from "../redux-arch/app/action"
+import { useAppDispatch, useAppSelector }  from "../redux-arch/store"
 import ProductDetails from "../../../components/product"
+
 const ProductPage=()=>{
-const dispatch=useAppDispatch()
-const product=useAppSelector((store)=>store.appreducer.homePageData)    
-const loading=useAppSelector((store)=>store.appreducer.isLoading)
-console.log(product,'product')
+  const dispatch=useAppDispatch()
+  const product=useAppSelector((store)=>store.appreducer.homePageData)    
+  const loading=useAppSelector((store)=>store.appreducer.isLoading)
+  
+  console.log(product,'product')
 
-
-
-react.useEffect(()=>{
-        
-  dispatch(getProduct())
-      
-        
-    },[])
+  react.useEffect(()=>{
+    dispatch(getProduct());    
+  },[])
     
    
 
     return <>
     <h1>all vidios</h1>
-  
-    {
-     loading?<h2>...loading</h2>:<ProductDetails  data={product} />
-    }
-    
+      {
+        loading?<h2>...loading</h2>:<ProductDetails data={product} />
+      }
     </>
 }
 
